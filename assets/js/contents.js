3e14d09beceab3ed42627c869c71b7c97f2fbc57
Anusha Salian <saliananusha1994@gmail.com>
2023-08-30 14:28:44 +0530

    contents updated automtaion contents.js and contents.json

---
+++ assets/js/contents.js
@@ -0,0 +1,80 @@
+"use strict";
+
+let html;
+
+// Insert contents to page
+const insertContent = function (sectionDiv, element) {
+  sectionDiv.insertAdjacentHTML("beforeend", element);
+};
+
+// create paragraph and single list elements
+const createContent = function (sectionDiv, tagName, elementValue) {
+  html = `<${tagName}>${elementValue}</${tagName}>`;
+  insertContent(sectionDiv, html);
+};
+
+// Create list with hyperlink
+const createExpList = function (sectionDiv, expName, expLink) {
+  html = `<li><a class=${expName} href=${expLink}>${expName}</a></li>`;
+  insertContent(sectionDiv, html);
+};
+
+// Create list within a list
+// const createMultipleList = function (mainCoarse, subCoarse) {
+//   html = `<li>${mainCoarse}<ul><li>${subCoarse}</li></ul></li>`;
+//   insertContent(html);
+// };
+const createMultipleList = function (sectionDiv, mainCoarse, subCoarse) {
+  html = `<li>${mainCoarse}<ul>`;
+  //Multiple list for single mainCoarse
+  subCoarse.forEach((coarse) => {
+    html += `<li>${coarse}</li>`;
+  });
+  html += `</ul></li>`;
+  insertContent(sectionDiv, html);
+};
+
+//Create diffrent contents
+const createContents = function (sectionDiv, data, sectionName, tag) {
+  const section = data[sectionName];
+  // For experiment list
+  if (tag === "li+a") {
+    for (let exp in section) {
+      createExpList(sectionDiv, exp, section[exp]);
+    }
+    return;
+  }
+  // For target audience
+  if (tag === "li+li") {
+    for (let coarse in section) {
+      createMultipleList(sectionDiv, coarse, section[coarse]);
+    }
+    return;
+  }
+  // For introduction, objectives, coarse alignment
+  section.forEach((elValue) => {
+    createContent(sectionDiv, tag, elValue);
+  });
+};
+
+// Fetch different contents from contents.json file
+const displayContent = function (sectionName, tag) {
+  const sectionDiv = document.querySelector(`.${sectionName}`);
+  fetch("assets/json/contents.json")
+    .then((res) => res.json())
+    .then((data) => {
+      if (tag === "name") {
+        insertContent(sectionDiv, data[sectionName]);
+        return;
+      }
+      createContents(sectionDiv, data, sectionName, tag);
+    });
+};
+
+const displayDomainName = function (sectionName, tag) {
+  displayContent(sectionName, tag);
+};
+
+const displayLabName = function (sectionName, tag) {
+  displayContent(sectionName, tag);
+};
